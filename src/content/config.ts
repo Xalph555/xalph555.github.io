7817ae29ee4d8a4abf1aa56437a7c54995bcee03
Sean Lee <81959287+Xalph555@users.noreply.github.com>
2024-06-14 22:25:29 +1000

    Style project page and add ability to dynamically generate project cards

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,6 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
+
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
     type: 'content',
@@ -15,7 +16,39 @@ const postsCollection = defineCollection({
       tags: z.array(z.string())
     })
 });
+const projectCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    cardImage: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    description: z.string(),
+    techDetails: z.array(z.string()),
+    subtitle: z.string(),
+    gameEmbed: z.object({
+      iframeSrc: z.string(),
+      url: z.string(),
+      width: z.number(),
+      height: z.number(),
+    }).optional(),
+    heroImage: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }).optional(),
+    specifications: z.array(z.string()),
+    links: z.object({
+      itchio: z.string().optional(),
+      github: z.string().optional(),
+      bitbucket: z.string().optional(),
+      techDocs: z.string().optional(),
+    }),
+    summary: z.string(),
+  })
+});
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: postsCollection,
+  projects: projectCollection,
 };
