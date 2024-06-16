a8655a563b97e89eb52b139a926bb080da17d60e
Sean Lee <81959287+Xalph555@users.noreply.github.com>
2024-06-16 14:53:00 +1000

    Format files

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,38 +2,42 @@
 import { z, defineCollection } from "astro:content";
 
 const projectCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    projectDate: z.date(),
-    cardImage: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    description: z.string(),
-    techDetails: z.array(z.string()),
-    subtitle: z.string(),
-    gameEmbed: z.object({
-      iframeSrc: z.string(),
-      url: z.string(),
-      width: z.number(),
-      height: z.number(),
-    }).optional(),
-    heroImage: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }).optional(),
-    specifications: z.array(z.string()),
-    links: z.object({
-      itchio: z.string().optional(),
-      github: z.string().optional(),
-      bitbucket: z.string().optional(),
-      techDocs: z.string().optional(),
-    }),
-    summary: z.string(),
-  })
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		projectDate: z.date(),
+		cardImage: z.object({
+			url: z.string(),
+			alt: z.string(),
+		}),
+		description: z.string(),
+		techDetails: z.array(z.string()),
+		subtitle: z.string(),
+		gameEmbed: z
+			.object({
+				iframeSrc: z.string(),
+				url: z.string(),
+				width: z.number(),
+				height: z.number(),
+			})
+			.optional(),
+		heroImage: z
+			.object({
+				url: z.string(),
+				alt: z.string(),
+			})
+			.optional(),
+		specifications: z.array(z.string()),
+		links: z.object({
+			itchio: z.string().optional(),
+			github: z.string().optional(),
+			bitbucket: z.string().optional(),
+			techDocs: z.string().optional(),
+		}),
+		summary: z.string(),
+	}),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  projects: projectCollection,
+	projects: projectCollection,
 };
